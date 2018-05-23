91a75b580bab8ba2d920dfd44da674ad728b1e7d
jPursey-UoM <jpursey@student.unimelb.edu.au>
2018-05-23 14:22:03 +1000

    first simple http request

--- app/src/main/java/com/example/jason/podcast/MainActivity.java
+++ app/src/main/java/com/example/jason/podcast/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.jason.podcast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView output = (TextView) findViewById(R.id.outputText);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://itunes.apple.com/lookup?id=909253";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        output.setText(response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(stringRequest);
     }
+
+
 }
